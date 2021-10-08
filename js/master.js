ab907acd83b05152f53224977b32299b53fee92e
smlee <smlee@gridone.co.kr>
2021-10-08 17:09:07 +0900

    초안 최최종

--- js/master.js
+++ js/master.js
@@ -15,3 +15,28 @@ $(document).ready(function() {
                 'color: #E68011;', 'color: #000;', 'color: #E68011;');
     /*$('html').animate({scrollTop : 0},10);*/
 });
+
+var theme = "light";
+
+function themeChange() {
+    if(theme == "light"){
+        theme = "dark";
+        $("img#theme-icon").attr("src","svg/moon-solid.svg");
+
+        $("div#aida-section").css("background-color", "#000000");
+        $("div#aida-section span").css("color", "#ffffff");
+        $("div#aida-section span").css("border-color", "#ffffff");
+        $("div#aida-section b").css("color", "#ffffff");
+        $("svg#aida-icon .cls-3").css("fill","#ffffff");
+    }
+    else{
+        theme = "light";
+        $("img#theme-icon").attr("src","svg/moon-regular.svg");
+
+        $("div#aida-section").css("background-color", "#ffffff");
+        $("div#aida-section span").css("color", "#000000");
+        $("div#aida-section span").css("border-color", "#000000");
+        $("div#aida-section b").css("color", "#000000");
+        $("svg#aida-icon .cls-3").css("fill","#000000");
+    }
+}
